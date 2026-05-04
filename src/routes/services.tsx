4a9456f526301fbce402e1f5fd20ce46b2485b1f
AshleyNotch <ashleyrehan.notch@gmail.com>
2026-05-05 03:20:03 +0530

    feat: update workshop exterior image to webp format

--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
-import heroImg from "@/assets/workshop-exterior.jpg";
+import heroImg from "@/assets/workshop-exterior.webp";
 import roadworthyImg from "@/assets/service-roadworthy.jpg";
 import repairsImg from "@/assets/service-repairs.jpg";
 import logbookImg from "@/assets/service-logbook.jpg";
